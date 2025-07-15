2ef8193e15a4f0bcececd2d2dbfa7baba4c29e7f
cocosharms14 <sharmanthomas.james@gmail.com>
2025-07-15 20:05:27 +0100

    react installed

--- src/index.js
+++
@@ -1,9 +0,0 @@
-const {javascript} = require('./javascript.js')
-const module = new javascript 
-if (module.connect() === true) {
-    console.log('Connection successful')
-}
-else {
-     console.log('Connection unsuccessful');
-    
-}
--- src/javascript.test.js
+++
@@ -1,7 +0,0 @@
-const {javascript} = require('./javascript.js')
-
-test('test file connects to javascript', () => {
-     const module = new javascript()
-
-    expect(module.connect()).toBe(true)
-})
